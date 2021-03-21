e3c056efacc42df24a0f2925fb62abbe9eb74ab4
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-03-21 15:34:12 +0000

    Prepared app for heroku deployment

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+app.use(cors())
 
 let phonebook = [
   {
@@ -108,5 +111,5 @@ app.post("/api/persons", (request, response) => {
   response.json(entry)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
